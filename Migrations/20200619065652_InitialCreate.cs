be9d31df780f6f359ab1f6cddd3d4c8dceaa9d28
hiteple <hiteplegold@gmail.com>
2020-06-19 04:13:10 -0300

    Making migrations with EF Core

---
+++ Migrations/20200619065652_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api_sql_platzi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactSet",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactSet", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactSet");
+        }
+    }
+}
--- models/Contacts.cs
+++ models/Contacts.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api_sql_platzi.models
 {
     public class Contacts
     {
+        [Key]
         public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
